dcb7d828509da3d33e7639a47e3b4340bfee61f7
carlosr929214 <36950268+carlosr929214@users.noreply.github.com>
2022-03-28 02:35:12 -0600

    Página Actualizada
    Falta funcionalidad de formulario

--- house_market/src/page/contacts.js
+++ house_market/src/page/contacts.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Footer } from '../components/Footer/footer'
 import { Header } from '../components/Header/header'
-import { Catalogo_inicio } from '../components/home/Catalogo_inicio'
-import { FormBuscar } from '../components/contacts/FormBuscar'
-import { Galeria_Inicio } from '../components/home/Galeria_Inicio'
-import { Nosotros } from '../components/home/Nosotros'
-import { Direccion_empresa } from '../components/nosotros/Direccion_empresa'
+import { MapsContacto } from '../components/contacts/MapsContacto'
+import { Contactanos } from '../components/contacts/Contactanos'
+import { AreaContacto } from '../components/contacts/AreaContacto'
+import { Direccion_empresa_contacto } from '../components/contacts/Direccion_empresa_contacto'
 import { Banner_page } from '../components/vistas/Banner_page'
+
 
 export const Contacts = () => {
   return (
@@ -15,26 +15,14 @@ export const Contacts = () => {
 
         <Banner_page Page={"Contacto"} />
 
-        <Direccion_empresa /> 
+        <Contactanos />
 
-        <div className="Banner-inicio-contacto">
-            <div className="Banner-inicio-contacto__info">
-                <div className="Banner-inicio-contacto__info--aviso">
-                    <h2>Quiere Rentar su Casa</h2>
-                    <h4>Llámenos y anuncie su propiedad aquí</h4>
-                </div>
-                <div className="Banner-inicio-contacto__info--contacto">
-                    <h4>+504 9845-XXXX</h4>
-                    <h4>empresa@gmail.com</h4>
-                    <a href="entrada.html" className="boton boton--secundario informacion-Blog__boton">Contáctenos</a>
-                </div>
-            </div>
-        </div>
+        <MapsContacto />
 
-       
+        <Direccion_empresa_contacto />
 
-       
-
+        <AreaContacto />
+   
         <Footer />
    </>
   )
