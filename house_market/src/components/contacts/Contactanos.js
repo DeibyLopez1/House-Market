280842b0e38ce5ace58bed12ed802e743f6428cb
carlosr929214 <36950268+carlosr929214@users.noreply.github.com>
2022-03-28 02:30:50 -0600

    Componentes Actualizados

---
+++ house_market/src/components/contacts/Contactanos.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Contactanos = () => {
+    return(
+
+<div class="row Contactanos">
+    <div class="col-md-12">
+        <div class="breadcrumb-area">
+            <span class="li">
+                <a href="../">Inicio</a>
+                </span>
+            <span >{'>'}</span>
+            <span className="li">Contacto</span>
+        </div>
+    </div>
+    
+</div>
+    
+    
+     )
+}
